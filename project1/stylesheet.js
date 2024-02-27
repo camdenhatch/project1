fd6b5f582fdf2cbc8d670923a51958e851c40897
camdenhatch <camden.hatch@gmail.com>
2024-02-26 18:24:45 -0700

    Leanred how to refernce javascript sheet and get the navbar to shrink when the page scrolls down, did this, used new favicon, and added a .js file to folder with javascript given by chat GPT (commented out when given by chat)

---
+++ project1/stylesheet.js
@@ -0,0 +1,14 @@
+// navbar shrink stuff given structure by a response to this
+// prompt in chat GPT in order to get the following JavaScript:
+
+// "make the navbar in the following code shrink when scrolling down 50px please:"
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbarLogo").classList.add("shrink");
+    } else {
+        document.getElementById("navbarLogo").classList.remove("shrink");
+    }
+}
